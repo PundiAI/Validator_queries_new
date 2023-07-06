361ed63d912a93b90a1e1c751c8dc7fa70a7c80c
malcolmtan <zherui_tan@live.com.sg>
2023-07-06 11:08:33 +0800

    Add start/stop indicators

--- index.py
+++ index.py
@@ -10,12 +10,14 @@ def main():
 
 
     # ---------------------------------------------main report---------------------------------------------
+    print("Generating report...")
     data = Data.build_validator_info()
     columns=["operator_address","moniker","jailed","total_delegated_tokens",
     "self_bonded_tokens", "peer_delegated_tokens",
     "outstanding_commission","outstanding_rewards","withdrawn_commission","withdrawn_rewards",
     "total_commission","total_rewards","total_earnings"]
     File._write_to_csv(data,"Test_Validator_Earnings_Report.csv",columns)
+    print("Report generated")
 
 
 if __name__ == '__main__':
