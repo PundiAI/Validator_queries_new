7cd2577c4fb8563415cfe14fbeb43b99cfb1e498
malcolmtan <zherui_tan@live.com.sg>
2023-07-06 11:02:29 +0800

    Breakdown total delegations to self and peer

--- index.py
+++ index.py
@@ -12,9 +12,10 @@ def main():
     # ---------------------------------------------main report---------------------------------------------
     data = Data.build_validator_info()
     columns=["operator_address","moniker","jailed","total_delegated_tokens",
+    "self_bonded_tokens", "peer_delegated_tokens",
     "outstanding_commission","outstanding_rewards","withdrawn_commission","withdrawn_rewards",
     "total_commission","total_rewards","total_earnings"]
-    File._write_to_csv(data,"Validator_Earnings_Report.csv",columns)
+    File._write_to_csv(data,"Test_Validator_Earnings_Report.csv",columns)
 
 
 if __name__ == '__main__':
