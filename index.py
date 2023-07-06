8c941e26c296a364ebf99920fb338f5ce6170308
malcolmtan <zherui_tan@live.com.sg>
2023-07-06 11:09:27 +0800

    Update file name to original

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ def main():
     "self_bonded_tokens", "peer_delegated_tokens",
     "outstanding_commission","outstanding_rewards","withdrawn_commission","withdrawn_rewards",
     "total_commission","total_rewards","total_earnings"]
-    File._write_to_csv(data,"Test_Validator_Earnings_Report.csv",columns)
+    File._write_to_csv(data,"Validator_Earnings_Report.csv",columns)
     print("Report generated")
 
 
